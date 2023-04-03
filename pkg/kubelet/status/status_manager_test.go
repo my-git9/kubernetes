d18c7a8f810f40602bd8d5773b16a3a398327ae6
xin.li <xin.li@daocloud.io>
2023-04-03 23:51:47 +0800

    fds
    Signed-off-by: xin.li <xin.li@daocloud.io>

--- pkg/kubelet/status/status_manager_test.go
+++ pkg/kubelet/status/status_manager_test.go
@@ -29,7 +29,6 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/stretchr/testify/assert"
-
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
